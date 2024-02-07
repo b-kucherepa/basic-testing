7fffaaedd7c69a100cde3046d9055476aeb49eac
b-kucherepa <bkuchr@gmail.com>
2024-02-07 19:48:38 +0400

    test: rework error handling tests with a proper Jest functionality

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -3,52 +3,30 @@ import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCusto
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
     const value = 'yay!';
-    const result = await resolveValue(value);
-    expect(result).toEqual(value);
+    await expect(resolveValue(value)).resolves.toEqual(value);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     const message = 'yay!';
-    let caughtError = null;
-    try {
-      throwError(message);
-    }
-    catch (error) {
-      caughtError = error;
-    }
-    expect(caughtError).toEqual(new Error(message));
+    expect(() => throwError(message)).toThrow(message);
   });
 
   test('should throw error with default message if message is not provided', () => {
     const defaultMessage = 'Oops!';
-    let caughtError = null;
-    try {
-      throwError();
-    }
-    catch (error) {
-      caughtError = error;
-    }
-    expect(caughtError).toEqual(new Error(defaultMessage));
+    expect(() => throwError(defaultMessage)).toThrow(defaultMessage);
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    let caughtError = null;
-    try {
-      throwCustomError();
-    }
-    catch (error) {
-      caughtError = error;
-    }
-    expect(caughtError).toBeInstanceOf(MyAwesomeError);
+    expect(() => throwCustomError()).toThrow(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    await expect(rejectCustomError()).rejects.toBeInstanceOf(MyAwesomeError);
+    await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
